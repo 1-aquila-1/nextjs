453d70be84448afb0233594235cac36cac846152
1-aquila-1 <aquilla.tavares7@gmail.com>
2022-02-08 19:17:03 -0300

    .

--- 01-curso/05-modulo/pages/alunos/[id].jsx
+++ 01-curso/05-modulo/pages/alunos/[id].jsx
@@ -1,11 +1,14 @@
 export async function getStaticPaths() {
+    const resp = await fetch('http://localhost:3000/api/alunos/tutores')
+    const ids = await resp.json()
+
+    const paths = ids.map(id => {
+        return { params: { id: `${id}` } }
+    })
+
     return {
-        fallback: false,
-        paths: [
-            {params: {id: '10'}},
-            {params: {id: '20'}},
-            {params: {id: '30'}}
-        ]
+        fallback: true, // false => 404
+        paths
     }
 }
 
@@ -13,6 +16,7 @@ export async function getStaticProps({ params }) {
     console.log(params)
     const resp = await fetch(`http://localhost:3000/api/alunos/${params.id}`)
     const aluno = await resp.json()
+
     return {
         props: {
             aluno
@@ -36,46 +40,3 @@ export default function AlunoPorId(props) {
         </div>
     )
 }
-
-// export async function getStaticPaths() {
-//     const resp = await fetch('http://localhost:3000/api/alunos/tutores')
-//     const ids = await resp.json()
-
-//     const paths = ids.map(id => {
-//         return { params: { id: `${id}` } }
-//     })
-
-//     return {
-//         fallback: true, // false => 404
-//         paths
-//     }
-// }
-
-// export async function getStaticProps({ params }) {
-//     console.log(params)
-//     const resp = await fetch(`http://localhost:3000/api/alunos/${params.id}`)
-//     const aluno = await resp.json()
-
-//     return {
-//         props: {
-//             aluno
-//         }
-//     }
-// }
-
-// export default function AlunoPorId(props) {
-//     const { aluno } = props
-//     return (
-//         <div>
-//             <h1>Detalhes do Aluno</h1>
-//             {aluno ?
-//                 <ul>
-//                     <li>{aluno.id}</li>
-//                     <li>{aluno.nome}</li>
-//                     <li>{aluno.email}</li>
-//                 </ul>
-//                 : false
-//             }
-//         </div>
-//     )
-// }
